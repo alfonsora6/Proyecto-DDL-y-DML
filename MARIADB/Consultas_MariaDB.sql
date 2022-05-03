b1fd19d9bc26b3d3e1844e90fba2847f56dc0398
Alfonso Roldán <megaalfonso84@gmail.com>
2022-05-03 16:25:49 +0200

    Consultas_MariaDB.sql añadido

---
+++ MARIADB/Consultas_MariaDB.sql
@@ -0,0 +1,19 @@
+--1- Consultas sencillas.
+
+-- Mostrar el nombre de los futbolistas que son delantero centro.
+SELECT Nombre FROM Jugadores WHERE Posicion='DC';
+
+-- Mostrar los equipos de la ciudad de Sevilla.
+SELECT Nombre FROM Equipos WHERE Ciudad='Sevilla';
+
+--2- Vistas.
+
+-- Crea una vista que muestre el nombre y la posición de los jugadores que nunca han metido un gol.
+
+CREATE VIEW Jugadores_sin_goles
+AS
+SELECT Nombre,Posicion 
+FROM Jugadores 
+WHERE Codigo NOT IN (SELECT CodJugador from Goles);
+
+SELECT * FROM Jugadores_sin_goles;
