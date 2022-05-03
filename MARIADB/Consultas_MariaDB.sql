05f8ce8143e2874be14fdee0c60658dbc69f8d06
Alfonso Roldán <megaalfonso84@gmail.com>
2022-05-03 16:48:57 +0200

    Consultas_MariaDB.sql modificado

--- MARIADB/Consultas_MariaDB.sql
+++ MARIADB/Consultas_MariaDB.sql
@@ -51,3 +51,14 @@ WHERE NombreEquipo=(SELECT Nombre
 SELECT CONCAT(p.Nombre," ",p.Apellido) AS "Nombre y apellido", e.Nombre 
 FROM Presidente p,Equipos e 
 WHERE p.NombreEquipo=e.Nombre;
+
+
+--5- Inserción de registros. Consultas de datos anexados.
+
+-- Inserta un nuevo jugador con código 123, con nombre 'David Alaba', su fecha de nacimiento es el 24 de junio de 1992, juega como defensa central, tiene el dorsal con el número 4, y que juega en el mismo equipo que 'Vinicius Junior'.
+INSERT INTO Jugadores
+SELECT 123,'David Alaba',STR_TO_DATE('24/06/1992','%d/%m/%Y'),'DFC',4,Nombre 
+FROM Equipos 
+WHERE nombre=(SELECT NombreEquipo 
+              FROM Jugadores 
+              WHERE nombre='Vinicius Junior');
