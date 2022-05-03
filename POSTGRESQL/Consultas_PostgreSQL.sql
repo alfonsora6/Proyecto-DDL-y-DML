4aa28edebf83273fcd20e529d2a2e28b9bdc7300
Alfonso Roldán <megaalfonso84@gmail.com>
2022-05-03 17:43:23 +0200

    Consultas_PostgreSQL.sql modificado

---
+++ POSTGRESQL/Consultas_PostgreSQL.sql
@@ -0,0 +1,7 @@
+--1- Consultas sencillas.
+
+-- Mostrar el nombre de los futbolistas que son delantero centro.
+SELECT Nombre FROM Jugadores WHERE Posicion='DC';
+
+-- Mostrar los equipos de la ciudad de Sevilla.
+SELECT Nombre FROM Equipos WHERE Ciudad='Sevilla';
